9ad6720021ba340697312387b441738a9bab2822
Yemi <yemibox51@gmail.com>
2023-12-21 00:11:35 -0500

    Added lualine plugin

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-			ensure_installed = {"lua","javascript","swift","cpp","go","python","c","html","java", "c_sharp"},
+			ensure_installed = {"lua","javascript","swift","cpp","go","python","c","html","java", "c_sharp", "markdown"},
 			highlight = { enable = true },
 			indent = { enable = true },
 		})
