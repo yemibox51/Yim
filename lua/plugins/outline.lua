d1012d502cd619dd99a2cf65966226e6c9cccad8
Yemibox <yemibox51@gmail.com>
2024-01-03 23:17:29 -0500

    Added Symbols Outline

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,7 @@
+return {
+    "simrat39/symbols-outline.nvim",
+     config = function()
+        require("symbols-outline").setup({})
+		vim.keymap.set('n', '<leader>o', ':SymbolsOutline<CR>', {})
+     end
+}
