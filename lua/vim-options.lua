3f1af41b479e6e0ea70021404ade646699523c45
Yemibox <yemibox51@gmail.com>
2024-01-04 20:11:44 -0500

    Added keybindings to resize, copy & paste to clipboard and to have neotree toggle

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,17 @@ vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
 vim.cmd("set expandtab")
 vim.g.mapleader = " "
+
+-- Resize Panes Faster --
+vim.keymap.set('n', '<C-j>', ':resize +2<CR>', {})
+vim.keymap.set('n', '<C-k>', ':resize -2<CR>', {})
+vim.keymap.set('n', '<C-h>', ':vertical resize +2<CR>', {})
+vim.keymap.set('n', '<C-l>', ':vertical resize -2<CR>', {})
+
+-- Copy & Paste to Clipboard --
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {}) -- Copy Cursor
+vim.keymap.set({'n', 'v'}, '<leader>Y', '"+Y', {}) -- Copy Line
+vim.keymap.set('n', '<leader>p', '"+p', {}) -- Paste After Cursor
+vim.keymap.set('n', '<leader>P', '"+P', {}) -- Paste Before Cursor
+vim.keymap.set({'n', 'v'}, '<leader>d', '"+d', {}) -- Cut Cursor
+vim.keymap.set({'n', 'v'}, '<leader>D', '"+D', {}) -- Cut Line
