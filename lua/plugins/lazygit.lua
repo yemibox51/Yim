8da0ea6554265d196d72d693ab843905c413618c
Yemi Agesin <yemi.agesin@cfacorp.com>
2024-06-28 15:22:24 -0400

    Added lazygit into nvim

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,15 @@
+return {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+  }
+}
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,13 @@ return {
 		tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
+            require("telescope").setup {
+                pickers = {
+                    find_files = {
+                      hidden = true,
+                    }
+                },
+            }
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
