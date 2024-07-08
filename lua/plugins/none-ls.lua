91ce67f914da25fb4c7503b48e49d4112c4edfb9
Yemi <yemibox51@gmail.com>
2024-07-07 20:46:45 -0500

    Added autopairs, comments, autotags, and web dev formatter

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,7 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.csharpier,
 				null_ls.builtins.formatting.gdformat,
+				null_ls.builtins.formatting.prettier,
 			},
 		})
 
