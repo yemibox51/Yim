3531a963d9667228ae3e37b93662ed36e1c22e0c
Yemi <yemibox51@gmail.com>
2024-07-13 11:41:19 -0400

    Added formatter and keybinding for it

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,6 +1,7 @@
 return {
 	"nvimtools/none-ls.nvim",
 	config = function()
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
@@ -9,8 +10,23 @@ return {
 				null_ls.builtins.formatting.gdformat,
 				null_ls.builtins.formatting.prettier,
 			},
-		})
 
-		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {});
+            -- Format on Save --
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({async = false})
+                        end,
+                    })
+                end
+            end,
+		})
+		vim.keymap.set('n', '<leader>gF', vim.lsp.buf.format, {});
 	end
 }
