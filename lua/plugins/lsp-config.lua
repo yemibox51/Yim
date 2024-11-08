79cb1637a99d3f7b3ed3bcaf3663924e5d730921
Yemi Agesin <yemi.agesin@cfacorp.com>
2024-11-08 13:49:04 -0500

    Added keymaps to switch tabs faster

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,73 +1,73 @@
 return {
-	{
+    {
         "williamboman/mason.nvim",
-		config = function()
+        config = function()
             require("mason").setup({})
         end
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"tsserver",
-					"pyright",
-					"gopls",
-				}
-			})
-		end
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "tsserver",
+                    "pyright",
+                    "gopls",
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
+            local lspconfig = require("lspconfig")
 
-			-- Language Servers --
-			lspconfig.lua_ls.setup({
+            -- Language Servers --
+            lspconfig.lua_ls.setup({
                 capabilities = capabilities,
             })
-			lspconfig.pyright.setup({
+            lspconfig.pyright.setup({
                 capabilities = capabilities,
             })
-			lspconfig.tsserver.setup({
+            lspconfig.tsserver.setup({
                 capabilities = capabilities,
             })
 
-              -- Godot --
+            -- Godot --
             if lspconfig.gdscript then
                 lspconfig.gdscript.setup({
-                capabilities = capabilities,
-             })
+                    capabilities = capabilities,
+                })
             end
             lspconfig.gopls.setup({
                 capabilities = capabilities,
             })
 
-			-- Keymaps --
-			vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
-			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-			vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
-			vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
-		end
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		-- This is your opts table
-		config = function()
-			require("telescope").setup {
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown {
-						}
-					}
-				}
-			}
-			require("telescope").load_extension("ui-select")
-		end
-	}
+            -- Keymaps --
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+        end
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        -- This is your opts table
+        config = function()
+            require("telescope").setup {
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown {
+                        }
+                    }
+                }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+    }
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -11,22 +11,25 @@ vim.keymap.set('n', '<C-h>', ':vertical resize +2<CR>', {})
 vim.keymap.set('n', '<C-l>', ':vertical resize -2<CR>', {})
 
 -- Copy & Paste to Clipboard --
-vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', {}) -- Copy Cursor
-vim.keymap.set({'n', 'v'}, '<leader>Y', '"+Y', {}) -- Copy Line
-vim.keymap.set('n', '<leader>p', '"+p', {}) -- Paste After Cursor
-vim.keymap.set('n', '<leader>P', '"+P', {}) -- Paste Before Cursor
-vim.keymap.set({'n', 'v'}, '<leader>d', '"+d', {}) -- Cut Cursor
-vim.keymap.set({'n', 'v'}, '<leader>D', '"+D', {}) -- Cut Line
+vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', {}) -- Copy Cursor
+vim.keymap.set({ 'n', 'v' }, '<leader>Y', '"+Y', {}) -- Copy Line
+vim.keymap.set('n', '<leader>p', '"+p', {})        -- Paste After Cursor
+vim.keymap.set('n', '<leader>P', '"+P', {})        -- Paste Before Cursor
+vim.keymap.set({ 'n', 'v' }, '<leader>d', '"+d', {}) -- Cut Cursor
+vim.keymap.set({ 'n', 'v' }, '<leader>D', '"+D', {}) -- Cut Line
 
--- Find & Replace -- 
-vim.keymap.set('n', '<leader>f', ':.,', {}) -- Replace to $/n in File
+-- Find & Replace --
+vim.keymap.set('n', '<leader>f', ':.,', {})  -- Replace to $/n in File
 vim.keymap.set('n', '<leader>F', ':%s/', {}) -- Replace All in File
 
 -- Tabs --
 vim.keymap.set('n', '<leader>T', ':tabnew<CR>', {})
+vim.api.nvim_set_keymap('n', '<Tab>', ':tabnext<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Tab>', ':tabprevious<CR>', { noremap = true, silent = true })
+
 
 -- Connect to Godot --
-local gdproject = io.open(vim.fn.getcwd().."/project.godot", "r")
+local gdproject = io.open(vim.fn.getcwd() .. "/project.godot", "r")
 if gdproject then
     io.close(gdproject)
     vim.fn.serverstart("./godothost")
