dbec4ddcf5125a6c3079547f5204162a5172f94d
Yemibox <yemibox51@gmail.com>
2024-11-15 23:31:55 -0500

    Commented out format on save

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,32 +1,32 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
+    "nvimtools/none-ls.nvim",
+    config = function()
         local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.csharpier,
-				null_ls.builtins.formatting.gdformat,
-				null_ls.builtins.formatting.prettier,
-			},
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = {
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.csharpier,
+                null_ls.builtins.formatting.gdformat,
+                null_ls.builtins.formatting.prettier,
+            },
 
             -- Format on Save --
-            on_attach = function(client, bufnr)
-                if client.supports_method("textDocument/formatting") then
-                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-                    vim.api.nvim_create_autocmd("BufWritePre", {
-                        group = augroup,
-                        buffer = bufnr,
-                        callback = function()
-                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                            vim.lsp.buf.format({async = false})
-                        end,
-                    })
-                end
-            end,
-		})
-		vim.keymap.set('n', '<leader>gF', vim.lsp.buf.format, {});
-	end
+            -- on_attach = function(client, bufnr)
+            --     if client.supports_method("textDocument/formatting") then
+            --         vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            --         vim.api.nvim_create_autocmd("BufWritePre", {
+            --             group = augroup,
+            --             buffer = bufnr,
+            --             callback = function()
+            --                 -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+            --                 -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+            --                 vim.lsp.buf.format({async = false})
+            --             end,
+            --         })
+            --     end
+            -- end,
+        })
+        vim.keymap.set('n', '<leader>gF', vim.lsp.buf.format, {});
+    end
 }
