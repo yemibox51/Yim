02bb1cc77c6011707960e97b6ed9084837df621d
Yemi Agesin <yemi.agesin@cfacorp.com>
2024-11-27 13:49:32 -0500

    Added renaming and diagnostic keymaps

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -46,13 +46,22 @@ return {
                 capabilities = capabilities,
             })
 
+            -- Protobuf --
+            if lspconfig.bufls then
+                lspconfig.bufls.setup({
+                    capabilities = capabilities
+                })
+            end
+
             -- Keymaps --
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
+            vim.keymap.set('n', 'gR', vim.lsp.buf.rename, {})
+            vim.keymap.set('n', '<leader>q', vim.diagnostic.open_float, {})
         end
     },
     {
