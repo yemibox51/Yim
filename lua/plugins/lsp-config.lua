5469e5966b52625bd7854c0449702400ea6d7249
Yemi Agesin <yemi.agesin@cfacorp.com>
2024-12-06 17:52:36 -0500

    Added signature floating while writing function

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -57,6 +57,7 @@ return {
             vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('i', '<C-K>', vim.lsp.buf.signature_help, {})
             vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
             vim.keymap.set('n', 'gr', vim.lsp.buf.references, {})
--- todo-neovim.md
+++ todo-neovim.md
@@ -4,3 +4,8 @@
 - [X] Code Snippets
 - [ ] Auto-completion
 - [ ] Formatting
+
+## Storage (for later, if needed)
+- [Signature](https://github.com/ray-x/lsp_signature.nvim)
+- NVChad Plugins
+- Lunar Plugins
