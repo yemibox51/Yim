4470f23109c67871d6c1f39a0c25d46091e62fdc
Yemibox <yemibox51@gmail.com>
2025-11-05 00:22:50 -0500

    Added telescope marks and document symbols mappings, deleted outline plugin

--- lua/plugins/outline.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "simrat39/symbols-outline.nvim",
-     config = function()
-        require("symbols-outline").setup({})
-		vim.keymap.set('n', '<leader>o', ':SymbolsOutline<CR>', {})
-     end
-}
