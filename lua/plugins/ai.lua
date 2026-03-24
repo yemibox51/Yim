e1d8d3e1a0457aa14e97137e789d9cf3cbe9ccb9
Yemi Agesin <yemi.agesin@cfacorp.com>
2026-03-24 15:35:42 -0400

    Removed conflicting tab

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -16,17 +16,6 @@ return {
       },
       keys = {
         {
-          "<tab>",
-          function()
-            -- if there is a next edit, jump to it, otherwise apply it if any
-            if not require("sidekick").nes_jump_or_apply() then
-              return "<Tab>" -- fallback to normal tab
-            end
-          end,
-          expr = true,
-          desc = "Goto/Apply Next Edit Suggestion",
-        },
-        {
           "<A-.>",
           function() require("sidekick.cli").toggle() end,
           mode = { "n", "t" },
